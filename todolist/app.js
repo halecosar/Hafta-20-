e2b21c0d06056fd4d6dbc19124be0f99b903a596
halecosar <halealtunakar@hotmail.com>
2024-01-25 22:54:13 +0300

    first

---
+++ todolist/app.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const liveToastBtn = document.getElementById("liveToastBtn");
+
+    liveToastBtn.addEventListener("click", function () {
+        addNewElement();
+    });
+})
+
+
+function addNewElement() {
+    const value = document.getElementById("task").value;
+    const list = document.getElementById("list");
+
+    if (value != null && value.trim() != "") {
+        const li = document.createElement("li");
+        li.appendChild(document.createTextNode(value));
+
+        const removeBtn = document.createElement("button")
+        removeBtn.textContent = "Sil";
+        removeBtn.style.marginLeft = "auto";
+
+        removeBtn.addEventListener("click", function () {
+            this.parentElement.remove();
+        });
+
+        var x = document.createElement("INPUT");
+        x.setAttribute("type", "checkbox");
+
+
+        li.appendChild(x);
+        li.appendChild(removeBtn);
+        list.appendChild(li);
+
+        document.getElementById("task").value = "";
+    }
+    else {
+        alert("Listeye boş eleman eklenemez!")
+    }
+}
+
